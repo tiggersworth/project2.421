2c9372c69201d28451905329b2ceac7b738b5267
tcraven96 <tjcraven@buffalo.edu>
2018-04-16 13:13:28 -0400

    Skeleton Code for Syscall Handler
    I create a skeleton code case-switch function for the syscall handler where each case is a different system call to be handled. I did the small amount of code required for halt().

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -15,6 +15,77 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+  //Need to create a function to check if it is a valid pointer
+  switch (* (int *) f->esp){
+	  case SYS_HALT:
+		  {
+			  shutdown_power_off();
+			  break;
+		  }
+	  case SYS_EXIT:
+		  {
+			  //What sys_exit will do
+			  break;
+		  }
+          case SYS_EXEC:
+		  {
+			  //What sys_exec will do
+			  break;
+		  }
+	  case SYS_WAIT:
+		  {
+			  //What sys_wait will do
+			  break;
+		  }
+          case SYS_CREATE:
+		  {
+			  //What sys_create will do
+			  break;
+		   }
+          case SYS_REMOVE:
+		  {
+			  //What sys_remove will do
+			  break;
+			   }
+          case SYS_OPEN:
+		  {
+			  //What sys_open will do
+			  break;
+			   }
+          case SYS_FILESIZE:
+		  {
+			  //What sys_filesize will do
+			  break;
+			   }
+          case SYS_READ:
+		  {
+			  //What sys_read will do
+			  break;
+			   }
+          case SYS_WRITE:
+		  {
+			  //What sys_write will do
+			  break;
+			   }
+          case SYS_SEEK:
+		  {
+			  //What sys_seek will do
+			  break;
+			   }
+          case SYS_TELL:
+		  {
+			  //What sys_tell will do
+			  break;
+		   }
+          case SYS_CLOSE:
+		  {
+			  //What sys_close will do
+			  break;
+		  }
+          default:
+		  {
+		  printf ("system call not available!\n");
+		  thread_exit ();
+		  }
 }
+
