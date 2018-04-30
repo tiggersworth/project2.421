77fe6fd8c84d0227b3faa82df4e5b058c849e991
sunnyxu715 <31707151+sunnyxu715@users.noreply.github.com>
2018-04-30 08:20:09 -0400

    added to thread struc for agr passing

--- threads/thread.h
+++ threads/thread.h
@@ -96,6 +96,8 @@ struct thread
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
+    char *command_args;
+
 #endif
 
     /* Owned by thread.c. */
