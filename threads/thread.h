d19c6271710fb9f758e8d5a8957f375ff2d86223
sunnyxu715 <31707151+sunnyxu715@users.noreply.github.com>
2018-05-11 17:45:23 -0400

    syscall

--- threads/thread.h
+++ threads/thread.h
@@ -125,7 +125,7 @@ struct thread *thread_current (void);
 tid_t thread_tid (void);
 const char *thread_name (void);
 
-void thread_exit (void) NO_RETURN;
+void thread_exit (int s) NO_RETURN;
 void thread_yield (void);
 
 /* Performs some operation on thread t, given auxiliary data AUX. */
